c5766886fcaefd8bdea9f9d96dcc106dee6d83aa
ayaneshsarkar <ayaneshsarkar101@gmail.com>
2024-01-26 01:46:31 +0530

    Finished with the project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    var timer: DispatchSourceTimer?
     
-    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var timer: DispatchSourceTimer?
+    var player: AVAudioPlayer!
+    let eggTimes: [String: Int] = ["Soft": 10, "Medium": 420, "Hard": 720]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness: String = sender.currentTitle!
@@ -20,9 +25,19 @@ class ViewController: UIViewController {
         func startCountdown() {
             timer?.cancel()
             
+            titleLabel.text = hardness
+            progressBar.progress = 0.0
+            
+            let totalTime = eggTimes[hardness]!
             var seconds = eggTimes[hardness]!
 
             timer = DispatchSource.makeTimerSource(queue: DispatchQueue.global())
+            
+            func playSound() {
+                let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
+            }
 
             timer?.schedule(deadline: .now(), repeating: 1.0)
             timer?.setEventHandler { [weak self] in
@@ -31,9 +46,21 @@ class ViewController: UIViewController {
                 if seconds > 0 {
                     print("\(seconds) seconds.")
                     seconds -= 1
+                    
+                    DispatchQueue.main.async {
+                        let currentProgress: Float = (Float(seconds) / Float(totalTime))
+                        self.progressBar.progress = (1.0 - currentProgress)
+                    }
                 } else {
                     print("Countdown finished")
                     self.timer?.cancel() // Stop the timer when the countdown is finished
+                    
+                    DispatchQueue.main.async {
+                        self.titleLabel.text = "DONE!"
+                        self.progressBar.progress = 1.0
+                        
+                        playSound()
+                    }
                 }
             }
 
